225d421eeee50c0434f49112b4e761e2154fb331
wandilf <36641609+wandilf@users.noreply.github.com>
2024-02-29 20:02:26 -0300

    Update KioskModeActivity.java

--- src/android/KioskModeActivity.java
+++ src/android/KioskModeActivity.java
@@ -99,11 +99,8 @@ public class KioskModeActivity extends CordovaActivity {
             this.getWindow()
                 .getDecorView()
                 .setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
                     View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                    View.SYSTEM_UI_FLAG_FULLSCREEN |
                     View.STATUS_BAR_VISIBLE |
                     View.SYSTEM_UI_FLAG_VISIBLE
                 );
